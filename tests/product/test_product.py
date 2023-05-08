fda6f04583c0325fe61cd41a52495fdf7a801398
caren <carenoliveirasilvapontes@gmail.com>
2023-05-08 15:04:40 -0300

    feat:testes de product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        id=1,
+        nome_do_produto="tênis",
+        nome_da_empresa="redley",
+        data_de_fabricacao="2023-05-08",
+        data_de_validade="2030-05-08",
+        numero_de_serie="08052023",
+        instrucoes_de_armazenamento="Mantenha longe de cachorros"
+    )
+
+    assert produto.id == 1
+    assert produto.nome_do_produto == "tênis"
+    assert produto.nome_da_empresa == "redley"
+    assert produto.data_de_fabricacao == "2023-05-08"
+    assert produto.data_de_validade == "2030-05-08"
+    assert produto.numero_de_serie == "08052023"
+    assert produto.instrucoes_de_armazenamento == "Mantenha longe de cachorros"
