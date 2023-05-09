1606854b6a38bf3994373916d0dcc34d397e9261
caren <carenoliveirasilvapontes@gmail.com>
2023-05-08 22:07:33 -0300

    feat:inventory refatorada para  seguir o padrão strategy

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,37 +1,8 @@
-import csv
-import json
-import xml.etree.ElementTree as Et
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-
-
-class FileC:
-    @staticmethod
-    def file_csv(path):
-        with open(path, mode="r", newline="", encoding="utf8") as file:
-            reader = csv.DictReader(file)
-            return [row for row in reader]
-
-
-class FileJ:
-    @staticmethod
-    def file_json(path):
-        with open(path, mode="r", encoding="utf8") as file:
-            return json.load(file)
-
-
-class FileX:
-    @staticmethod
-    def file_xml(path):
-        tree = Et.parse(path)
-        roots = tree.getroot()
-        stock = []
-        for root in roots:
-            item = {}
-            for subroot in root:
-                item[subroot.tag] = subroot.text
-            stock.append(item)
-        return stock
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
@@ -39,18 +10,16 @@ class Inventory:
     def read(path):
         path_especific = path.split(".")[-1]
         if path_especific == "csv":
-            return FileC.file_csv(path)
+            return CsvImporter.import_data(path)
         elif path_especific == "json":
-            return FileJ.file_json(path)
+            return JsonImporter.import_data(path)
         elif path_especific == "xml":
-            return FileX.file_xml(path)
+            return XmlImporter.import_data(path)
 
     @staticmethod
     def import_data(path, type):
         result = Inventory.read(path)
         if type == "simples":
-            report = SimpleReport.generate(result)
+            return SimpleReport.generate(result)
         elif type == "completo":
-            report = CompleteReport.generate(result)
-
-        return report
+            return CompleteReport.generate(result)
