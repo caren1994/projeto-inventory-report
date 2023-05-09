91760e30ffe5033055197fd556ac781324bd3d0d
caren <carenoliveirasilvapontes@gmail.com>
2023-05-08 22:22:40 -0300

    feat:test test_product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "tênis",
+        "redley",
+        "08/05/2023",
+        "08/05/2030",
+        "08052023",
+        "longe dos cachorros",
+    )
+
+    assert produto.__repr__() == (
+        f"O produto {produto.nome_do_produto}"
+        f" fabricado em {produto.data_de_fabricacao}"
+        f" por {produto.nome_da_empresa} com validade"
+        f" até {produto.data_de_validade}"
+        f" precisa ser armazenado {produto.instrucoes_de_armazenamento}."
+    )
